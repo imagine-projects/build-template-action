e3f90d791f5d084a5dc413910095fff0d05f6f73
Ariel Weinberger <weinberger.ariel@gmail.com>
2025-12-21 19:51:43 -0600

    include glob in bundle

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@
  * The entrypoint for the action. This file simply imports and runs the action's
  * main logic.
  */
+
+// Force bundle these dependencies that @e2b/code-interpreter requires at runtime
+import 'glob'
+
 import { run } from './main.js'
 
 /* istanbul ignore next */
